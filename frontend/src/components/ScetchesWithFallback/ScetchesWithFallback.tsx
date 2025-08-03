8791cfec6bf86f05827a2708c202d5e7846d5f38
ArickCodeGuy <arickmail1997@gmail.com>
2025-08-03 08:27:42 +0300

    small card update

--- frontend/src/components/ScetchesWithFallback/ScetchesWithFallback.tsx
+++ frontend/src/components/ScetchesWithFallback/ScetchesWithFallback.tsx
@@ -1,59 +1,41 @@
-import { KCard } from 'figmaclone-uikit';
+import { classNameArrayToString, KCard } from 'figmaclone-uikit';
 import type { ScetchesWithFallbackProps } from './types';
-import { useNavigate } from 'react-router-dom';
 import type { ScetchItem } from '../../api/ScetchesController/ScetchesController';
 import { replaceRouteParams } from '../../router/replaceRouteParams';
 
-export function ScetshesRow(
-  props: Required<Pick<ScetchesWithFallbackProps, 'scetches'>>
-) {
-  const { scetches } = props;
-  const navigate = useNavigate();
-
-  function navigateToScetch(scetch: ScetchItem) {
-    navigate(replaceRouteParams('SCETCH', { id: scetch.id }));
-  }
-
+export function Scetch({ scetch }: { scetch: ScetchItem }) {
   return (
-    <div className="krow">
-      {scetches.map((s) => (
-        <div className="kcol-lg-2">
-          <KCard title={s.name} onBottomClick={() => navigateToScetch(s)} />
-        </div>
-      ))}
-    </div>
+    <KCard
+      title={scetch.name}
+      link={replaceRouteParams('SCETCH', { id: scetch.id })}
+    />
   );
 }
 
-function ScetshesFallback() {
-  const navigate = useNavigate();
-
-  function navigateToNewScetch() {
-    navigate(replaceRouteParams('SCETCH', { id: -1 }));
-  }
-
+function ScetchCreate() {
   return (
-    <div className="krow">
-      <div className="kcol-lg-2">
-        <KCard
-          title="Create new scetch"
-          onBottomClick={() => navigateToNewScetch()}
-        />
-      </div>
-    </div>
+    <KCard
+      title="Create new scetch"
+      link={replaceRouteParams('SCETCH', { id: -1 })}
+    />
   );
 }
 
 export function ScetchesWithFallback(props: ScetchesWithFallbackProps) {
   const { scetches } = props;
 
+  const colClasses = classNameArrayToString(['kcol-lg-2']);
+
   return (
-    <div>
-      {scetches?.length ? (
-        <ScetshesRow scetches={scetches} />
-      ) : (
-        <ScetshesFallback />
-      )}
+    <div className="krow">
+      <div className={colClasses}>
+        <ScetchCreate />
+      </div>
+      {(scetches || []).map((s) => (
+        <div className={colClasses}>
+          <Scetch scetch={s} />
+        </div>
+      ))}
     </div>
   );
 }
