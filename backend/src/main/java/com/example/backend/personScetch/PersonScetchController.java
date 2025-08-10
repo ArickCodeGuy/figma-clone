c0954c972d5a4868ddab9dd9252fbb4e5250b1e6
ArickCodeGuy <arickmail1997@gmail.com>
2025-08-10 11:06:22 +0300

    swagger add

--- backend/src/main/java/com/example/backend/personScetch/PersonScetchController.java
+++ backend/src/main/java/com/example/backend/personScetch/PersonScetchController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.backend.scetch.ScetchDto;
 import com.example.backend.scetch.ScetchRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @Controller
 public class PersonScetchController {
   @Autowired
@@ -18,6 +20,7 @@ public class PersonScetchController {
   @Autowired
   private ScetchRepository scetchRepository;
 
+  @Operation(summary = "Get list of scetches allowed for view by user")
   @GetMapping("/api/v1/scetches")
   @ResponseBody
   public List<ScetchDto> getScetches() {
