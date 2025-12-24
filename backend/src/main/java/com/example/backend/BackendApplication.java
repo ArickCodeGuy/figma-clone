dfeae8a4049eab5ec25c71ff331501ff949739fa
ArickCodeGuy <arickmail1997@gmail.com>
2025-12-24 21:24:02 +0300

    small update

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	// http://localhost:5137/swagger-ui/index.html
+	// http://localhost:5137/swagger-ui.html
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(BackendApplication.class);
 
