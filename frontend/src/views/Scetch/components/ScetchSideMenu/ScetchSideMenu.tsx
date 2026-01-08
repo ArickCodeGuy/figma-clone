a25d3ed61c0e3844e77e9633c1ca3066aebf9cc6
ArickCodeGuy <arickmail1997@gmail.com>
2026-01-08 09:41:35 +0300

    small update

--- frontend/src/views/Scetch/components/ScetchSideMenu/ScetchSideMenu.tsx
+++ frontend/src/views/Scetch/components/ScetchSideMenu/ScetchSideMenu.tsx
@@ -1,6 +1,72 @@
 import { ScetchSideMenuProps } from './types';
 import './styles.scss';
+import {
+  KAccordion,
+  KButton,
+  KButtonProps,
+  KList,
+  KTitle,
+} from 'figmaclone-uikit';
 
 export function ScetchSideMenu(props: ScetchSideMenuProps) {
-  return <div className="ScetchSideMenu">ScetchSideMenu</div>;
+  const actions: KButtonProps[] = [
+    {
+      title: 'Choose & Drag',
+      size: 'MINI',
+      iconLeft: 'cursor-default',
+      onClick: () => {
+        console.log('Choose & Drag');
+      },
+    },
+    {
+      title: 'Move canvas',
+      size: 'MINI',
+      iconLeft: 'hand-back-left',
+      onClick: () => {
+        console.log('Move canvas');
+      },
+    },
+  ];
+
+  const figures: KButtonProps[] = [
+    {
+      title: 'Line',
+      size: 'MINI',
+      iconLeft: 'vector-line',
+      onClick: () => {
+        console.log('Line');
+      },
+    },
+    {
+      title: 'Circle',
+      size: 'MINI',
+      iconLeft: 'circle',
+      onClick: () => {
+        console.log('Circle');
+      },
+    },
+    {
+      title: 'Square',
+      size: 'MINI',
+      iconLeft: 'square',
+      onClick: () => {
+        console.log('Square');
+      },
+    },
+  ];
+
+  return (
+    <div className="ScetchSideMenu">
+      <div className="side-menu-list">
+        <div>
+          <KTitle> Actions </KTitle>
+          <KList component={KButton} items={actions} />
+        </div>
+        <div>
+          <KTitle> Figures </KTitle>
+          <KList component={KButton} items={figures} />
+        </div>
+      </div>
+    </div>
+  );
 }
